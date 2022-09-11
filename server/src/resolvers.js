f62f1b91e35c2b900b5d3a600d03b0053165e784
Andy Pickler <andypickler@rocketauto.com>
2022-09-11 05:04:42 -0600

    Completion of course

---
+++ server/src/resolvers.js
@@ -0,0 +1,14 @@
+const resolvers = {
+  Query: {
+    tracksForHome: (_parent, _arg, { dataSources }) => {
+      return dataSources.trackAPI.getTracksForHome();
+    },
+  },
+  Track: {
+    author: ({ authorId }, _, { dataSources }) => {
+      return dataSources.trackAPI.getAuthor(authorId);
+    },
+  },
+};
+
+module.exports = resolvers;
